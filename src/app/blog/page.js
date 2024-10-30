5cba885d6bf0a82e115acfe9990de50ff79848a2
Gr3enMil <gr3en@seznam.cz>
2024-10-30 07:04:49 +0100

    zadne carky a hacky

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import styles from "./blog.module.css";
+import Image from 'next/image';
+import matter from 'gray-matter';
 
 export default async function BlogPage() {
   const postsDirectory = path.join(process.cwd(), 'src/content/blog');
@@ -9,20 +11,44 @@ export default async function BlogPage() {
   const posts = files.map((filename) => {
     const filePath = path.join(postsDirectory, filename);
     const content = fs.readFileSync(filePath, 'utf8');
+    const { data, content: postContent } = matter(content); // Parsování pomocí gray-matter
     return {
       filename,
-      content,
+      ...data,       // Obsah frontmatter (title, date, image)
+      content: postContent, // Skutečný obsah příspěvku
     };
   });
 
+  console.log(posts);
+  
   return (
     <div className={styles.blogContainer}>
       <h1>Blog</h1>
-      <ul>
+      <ul className={styles.blogPost}>
         {posts.map((post, index) => (
-          <li key={index}>
-            <h2>{post.filename.replace('.md', '')}</h2>
-            <p>{post.content.slice(0, 100)}...</p> {/* Ukázka z obsahu */}
+          <li key={index} className={styles.blogItem}>
+            {/* Nadpis */}
+            <h2 style={{ textTransform: 'uppercase', fontWeight: 'bold' }}>
+              {post.title}
+            </h2>
+            
+            {/* Obrázek a úryvek textu */}
+            <div className={styles.blogContent}>
+              {/* Levá část - obrázek */}
+              <div className={styles.blogImage}>
+                <Image
+                  src={post.image} // z frontmatteru, např. /uploads/blog2.png
+                  alt={`Image for ${post.title}`} // SEO-friendly alt text
+                  width={100}
+                  height={100}
+                />
+              </div>
+              
+              {/* Pravá část - úryvek textu */}
+              <div className={styles.blogText}>
+                <p>{post.content.slice(0, 100)}...</p>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
