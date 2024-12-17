66c7ab7fc5d70bd1b328993480035c2a3e688f0a
Gr3enMil <gr3en@seznam.cz>
2024-12-17 11:17:56 +0100

    another one

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -6,9 +6,20 @@ import { getPosts } from '../../lib/getPosts';
 export default async function BlogPage() {
   const posts = await getPosts();
 
-  return ( 
+  return (
     <div className={styles.blogContainer}>
       <h1 className={styles.title}>Blog</h1>
+      <div className={styles.navMenu}>
+        <Link href="/" className={styles.blogLink}>
+          &lt; ZPĚT NA HOMEPAGE
+        </Link>
+        <div className={styles.filter}>
+          <div className={styles.filterButton}>TRÉNINK</div>
+          <div className={styles.filterButton}>STRAVA</div>
+          <div className={styles.filterButton}>LIFESTYLE</div>
+          <div className={styles.filterButton}>RECEPTY</div>
+        </div>
+      </div>
       <ul className={styles.blogPost}>
         {posts.map((post) => {
           if (!post.nazev) {
@@ -20,10 +31,9 @@ export default async function BlogPage() {
             <li key={post.id} className={styles.blogItem}>
               <Link href={`/blog/${post.nazev}`}>
                 {/* Nadpis */}
-                <h2 style={{ textTransform: 'uppercase', fontWeight: 'bold', marginBottom: '1rem' }}>
+                <h2 style={{ textTransform: 'uppercase'}}>
                   {post.title}
                 </h2>
-
                 {/* Obrázek a úryvek textu */}
                 <div className={styles.blogContent}>
                   {/* Levá část - obrázek */}
@@ -32,8 +42,8 @@ export default async function BlogPage() {
                       src={post.image} // z frontmatteru, např. /uploads/blog2.png
                       alt={`Image for ${post.title}`} // SEO-friendly alt text
                       style={{ objectFit: 'cover' }}
-                      width={100}
-                      height={100}
+                      width={148}
+                      height={171}
                     />
                   </div>
 
